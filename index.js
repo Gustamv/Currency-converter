b8d199e2c29d6e456b9a2c7b9620b83df936acf2
Gustavo Melo <gustamvieira@gmail.com>
2023-09-26 16:01:25 -0300

    index.js devidamente comentado

--- index.js
+++ index.js
@@ -1,50 +1,55 @@
-window.addEventListener('load', () => {
-  const selects = document.querySelectorAll('select');
+// Seleciona todos os elementos 'select' da página
+const selects = document.querySelectorAll('select');
 
-  // Supondo que 'currencies' seja um objeto com os códigos e nomes das moedas.
-  const currencies = {
-      BRL: 'Real (BRL)',
-      ARS: 'Peso Argentino (ARS)',
-      USD: 'Dólar Americano (USD)',
-      BTC: 'Bitcoin (BTC)'
-  };
+// Define as opções de moedas
+const currencies = {
+  BRL: 'Real (BRL)',
+  ARS: 'Peso Argentino (ARS)',
+  USD: 'Dólar Americano (USD)',
+  BTC: 'Bitcoin (BTC)'
+};
 
-  for (const select of selects) {
-      for (const currencyCode in currencies) {
-          if (currencies.hasOwnProperty(currencyCode)) {
-              const currencyName = currencies[currencyCode];
-              const option = document.createElement('option');
-              option.value = currencyCode;
-              option.textContent = currencyName;
+//Matriz para preencher as options
+// for...of que itera com os selects
+for (const select of selects) {
+    // for...in que percorre nas propriedades do objeto currencies
+    for (const currencyCode in currencies) {
+        // O currencyName recebe o codigo da moeda (ex: BRL, USD...)
+        const currencyName = currencies[currencyCode];
+    // Cria as options baseada na 'const currencies'
+        const option = document.createElement('option');
+        option.value = currencyCode;
+        option.textContent = currencyName;
+    // o appendChild torna visivel a seleção 
+        select.appendChild(option);
+    }
+}
 
-              select.appendChild(option);
-          }
-      }
-  }
-});
-
+// Manipula o formulário quando for enviado
 document.getElementById('currencyConverterForm').addEventListener('submit', function (event) {
   event.preventDefault(); // Impede o recarregamento da página
 
+  // Obtém os dados do formulário
   const amount = parseFloat(document.querySelector('.amount input').value);
   const fromCurrency = document.querySelector('.from select').value;
   const toCurrency = document.querySelector('.to select').value;
 
-  // Taxas de câmbio fixas
+  // Define as taxas de câmbio fixas
   const exchangeRates = {
-      BRL: { ARS: 72.1500722, USD: 0.2062408, BTC: 0.00000755, BRL: 1 },
-      ARS: { BRL: 0.01386	, USD: 0.0028576, BTC: 0.000001, ARS: 1 },
-      USD: { BRL: 4.8487011, ARS: 349.95, BTC: 0.00003687,USD: 1 },
-      BTC: { BRL: 132266.70, ARS: 9491891.35, USD: 27115.47, BTC: 1}
+    BRL: { ARS: 72.1500722, USD: 0.2062408, BTC: 0.00000755 },
+    ARS: { BRL: 0.01386, USD: 0.0028576, BTC: 0.000001 },
+    USD: { BRL: 4.8487011, ARS: 349.95, BTC: 0.00003687 },
+    BTC: { BRL: 132266.70, ARS: 9491891.35, USD: 27115.47 }
   };
 
   let result;
-
+  //condição para mudar as casas decimais caso seja em BTC
   if (toCurrency === 'BTC') {
-      result = (amount * exchangeRates[fromCurrency][toCurrency]).toFixed(8);
+    result = (amount * exchangeRates[fromCurrency][toCurrency]).toFixed(8);
   } else {
-      result = (amount * exchangeRates[fromCurrency][toCurrency]).toFixed(2);
+    result = (amount * exchangeRates[fromCurrency][toCurrency]).toFixed(2);
   }
 
+  // Exibe o resultado da conversão
   document.querySelector('.result').textContent = `${amount} ${fromCurrency} = ${result} ${toCurrency}`;
 });
